e6578f96d2ef04fa7d43ca78a0eb63ef26d2069c
Franco Martin Schimizzi <xchimicasla@hotmail.com>
2026-06-08 22:33:14 -0300

    feat (frontend): implementacion de un stack basico de navegacion

---
+++ frontend/src/screens/HomeScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { styles } from './HomeStyles';
+
+export default function HomeScreen ({ navigation }: any) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Bienvenido a Logitrack</Text>
+            <Text style={styles.subtitulo}>Panel Principal de Logistica</Text>
+            <Button
+                title="Cerrar Sesion"
+                onPress={() => navigation.navigate('Login')}
+            />
+        </View>
+    )
+}
---
+++ frontend/src/screens/HomeStyles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#eef2f3',
+    },
+    titulo: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#lalala'
+    },
+    subtitulo: {
+        fontSize: 16,
+        color: '#666',
+        marginBottom: 20,
+    }
+});
